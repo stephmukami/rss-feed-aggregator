5071cfc8ce89b85e2105add4fc2cbd8a55e2ecbc
stephmukami <stephmukami@gmail.com>
2025-01-13 09:46:53 +0300

    feature: add automatic env variable reading

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+)
 
 func main(){
 
 fmt.Println("hello")
 
+godotenv.Load(".env")
+
+portString := os.Getenv("PORT")
+
+if portString == ""{
+	log.Fatal("PORT is not found")
+}
+fmt.Println("Port: ",portString)
 }
