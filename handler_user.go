7cb90a9f78883a94c023cdd70f1452a7a3e04fe0
stephmukami <stephmukami@gmail.com>
2025-01-15 10:07:09 +0300

    feature: add custom model

--- handler_user.go
+++ handler_user.go
@@ -35,5 +35,7 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 			return
 		}
 
-	respondWithJSON(w,200,user)
+	// respondWithJSON(w,200,user)
+	respondWithJSON(w,200,databaseToUser(user))
+
 }
